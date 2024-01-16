6da0215239146d9529a713351c3f84a907efce8b
Nick Galvez <nicholas.galvez@gmail.com>
2024-01-16 12:29:02 -0800

    Initial Commit

---
+++ Menu2/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Menu2
+//
+//  Created by Nick Galvez on 1/16/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ Menu2UITests/Menu2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Menu2UITestsLaunchTests.swift
+//  Menu2UITests
+//
+//  Created by Nick Galvez on 1/16/24.
+//
+
+import XCTest
+
+final class Menu2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
